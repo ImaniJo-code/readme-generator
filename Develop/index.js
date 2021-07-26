1f166114ae37c17a30ba98a42bf459c45a25fb92
ImaniJo-code <imani.l.johnson@gmail.com>
2021-07-25 21:21:18 -0400

    user input questions added

--- Develop/index.js
+++ Develop/index.js
@@ -4,10 +4,130 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please enter the name of your project!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please enter a description for your project!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide info for installing your application. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide info for installing your application!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide info for using your application. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide info for using your application!');
+            return false;
+            }
+        } 
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Provide license info. (Required)',
+        choices: ['Apache', 'Common-Development-and Distribution', 'GNU-General-Public', 'MIT', 'Mozilla-Public', 'None'],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide licensing info for your application!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide info for how other can contribute to the application. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide info for contributing to your application!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide info for how to test your application. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide info for testing your application!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github Username. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide your Github username!');
+            return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+            console.log('Please provide your email address!');
+            return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
